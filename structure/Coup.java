9a05c3f75b98c8cf324262bde82e36bd9628ee19
Daemianas <lombar2d@laposte.net>
2021-05-18 16:09:30 +0200

    Commentaire des coups.

--- structure/Coup.java
+++ structure/Coup.java
@@ -5,30 +5,36 @@ public class Coup {
     Point arrive;
     Point construction;
 
+    // Crée un coup de positionnement.
     public Coup(Point dep){
         initial = dep;
         arrive = null;
         construction = null;
     }
 
+    // Crée un coup de déplacement.
     public Coup(Point dep, Point arr, Point cons){
         initial = dep;
         arrive = arr;
         construction = cons;
     }
 
+    // Récupère le point de départ d'un coup.
     public Point getDepart(){
         return initial;
     }
 
+    // Récupère le point d'arrivé du coup.
     public Point getArrive(){
         return arrive;
     }
 
+    // Récupère le point de construction du coup.
     public Point getConstruction(){
         return construction;
     }
     
+    // Retoure vrai si le coup est un déplacement.
     public Boolean estDeplacement(){
         return !(construction == null);
     }
