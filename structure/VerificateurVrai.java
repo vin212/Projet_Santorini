ddc2e0d7fe2275eefca8f52d198c7ce4604f0c26
Daemianas <lombar2d@laposte.net>
2021-05-26 09:09:41 +0200

    Ajout d'un vérificateur pour obtenir toutes les cases voisines.

---
+++ structure/VerificateurVrai.java
@@ -0,0 +1,15 @@
+package structure;
+
+import modele.Jeu;
+
+public class VerificateurVrai {
+    Jeu j;
+
+    public VerificateurVrai(Jeu jeu){
+        j = jeu;
+    }
+
+    public boolean verifie(Point p, Point p2){
+        return true;
+    }
+}
