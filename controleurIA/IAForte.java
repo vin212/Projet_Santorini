0d95d10fe8f5cf6022fd0bb5a82fc1cedd594475
Daemianas <lombar2d@laposte.net>
2021-06-03 15:57:47 +0200

    Ajout de commentaire et revu de l'heuristique.

--- controleurIA/IAForte.java
+++ controleurIA/IAForte.java
@@ -30,22 +30,29 @@ public class IAForte extends IAMinMax {
         if (h.victoireBlocage(tampon) == -1) {
             return 1000; // Le joueur dont c'est le tour ne peut pas bouger. Il perd.
         }
+        // Calcule des hauteurs de chacun des pions des joueurs.
         Integer hauteurJ1 = h.evaluationHauteur(p0);
+        valuation -= hauteurJ1 * 5; // hauteurJ1 < 5
         Integer hauteurJ2 = h.evaluationHauteur(p1);
+        valuation += hauteurJ2 * 3; // hauteurJ2 < 5
 
+        // Calcul du nombre de case qui peuvent faire monter
         Integer peutMonterJ1 = h.peutMonter(p0[0], successeurP1J1.get(0));
         peutMonterJ1 += h.peutMonter(p0[1], successeurP2J1.get(0));
+
         Integer peutMonterJ2 = h.peutMonter(p1[0], successeurP1J2.get(0));
         peutMonterJ2 += h.peutMonter(p1[1], successeurP2J2.get(0));
-        valuation -= peutMonterJ1 * 5;
-        valuation += peutMonterJ2 * 3;
+
+        valuation -= peutMonterJ1 * 5;        // peutMonterJ1 < 17
+        valuation += peutMonterJ2 * 3;        // peutMonterJ2 < 17
         
         ArrayList<Point> sommetsVictorieux = h.peutMonter3(tampon);
         if (sommetsVictorieux.size() >= 1){
-            return 1000; // Le joueur a au moins deux possibilités de gagner
+            return -1000; // Le joueur a au moins deux possibilités de gagner.
         }
         tampon = new ArrayList<Point>(successeurP1J2.get(0));
         tampon.addAll(successeurP2J2.get(0));
+
         sommetsVictorieux = h.peutMonter3(tampon);
         if (sommetsVictorieux.size() > 1){
             return 1000; // L'adversaire a au moins deux possibilités de gagner
@@ -54,7 +61,7 @@ public class IAForte extends IAMinMax {
             tampon.addAll(successeurP2J1.get(1));
             if (h.constructionAdverse(sommetsVictorieux.get(0), tampon)){
                 // On peut contrer la possible victoire de l'ennemie.
-                valuation -= 100;
+                return -1000;
             } else {
                 // On ne peut pas contrer la victoire de l'ennemie. Défaite assuré.
                 return 1000;
@@ -79,12 +86,12 @@ public class IAForte extends IAMinMax {
             if (h.peutBouger(successeurP2J2.get(0)) == 0){
                 // Situation compliqué à calculer, non pris en compte pour le moment.
             } else {
-                valuation -= 100;
+                valuation -= 50;
                 // L'adversaire a un pion bloqué
             }
         } else if (h.peutBouger(successeurP2J2.get(0)) == 0){
             // L'adversaire a l'autre pion bloqué
-            valuation -= 100;
+            valuation -= 50;
         }
         
         tampon = new ArrayList<Point>(successeurP1J1.get(1));
@@ -92,15 +99,15 @@ public class IAForte extends IAMinMax {
         Integer bloquant = h.constructionBloquante(p1, tampon);
         Integer bloquant3 = h.constructionBloquante3(p1, tampon);
 
-        valuation -= 2 * bloquant;
-        valuation -= 4 * bloquant3;
+        valuation -= 0 * bloquant;
+        valuation -= 5 * bloquant3;
 
         tampon = new ArrayList<Point>(successeurP1J2.get(1));
         tampon.addAll(successeurP2J2.get(1));
         ArrayList<Point> tampon2 = new ArrayList<Point>(successeurP1J1.get(1));
         tampon2.addAll(successeurP2J1.get(1));
         if(h.nonContester(tampon2,tampon) == 1){
-            valuation -= 400;
+            valuation -= 200;
         }
 
         /*
